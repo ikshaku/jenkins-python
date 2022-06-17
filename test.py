71c3858486c6fd34f6f45cf3e2e2cbd49f5798d2
IKSHAKU GOSWAMI <ikshakugoswami@IKSHAKUs-MacBook-Air.local>
2022-06-16 19:13:43 -0700

    read git json from python

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import json
+import requests
 
-with open('test.json') as f:
-   data = json.load(f)
-
-print(data['name'])
 # print ("Hello World!")
+
+url = 'https://raw.githubusercontent.com/ikshaku/jenkins-python/main/test.json'
+
+# with open('test.json') as f:
+#    data = json.load(f)
+
+resp = requests.get(url)
+data = json.loads(resp.text)
+print(data['name'])    
